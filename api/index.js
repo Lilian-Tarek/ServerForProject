b6fee3cd9a8e81b8c3d295ce678d48f766bdace2
Lilian <liliantarek4@gmail.com>
2026-03-16 01:03:56 +0200

    third

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const path = require("path"); // ضيفي السطر ده
 
 const app = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+// تعديل المسار هنا عشان Vercel يشوف الملف صح
+const router = jsonServer.router(path.join(__dirname, "../db.json"));
 
 app.db = router.db;
 
-app.use(jsonServer.defaults());
+const middlewares = jsonServer.defaults();
+
+app.use(middlewares);
 app.use(auth);
 app.use(router);
 
