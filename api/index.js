89f25b1e7ce61136c4ce891665f4d6d75dd7a0a0
Lilian <liliantarek4@gmail.com>
2026-03-17 11:37:38 +0200

    Third

--- api/index.js
+++ api/index.js
@@ -1,23 +1,50 @@
+// const jsonServer = require("json-server");
+// const auth = require("json-server-auth");
+// const path = require("path"); // ضيفي السطر ده
+
+// const app = jsonServer.create();
+
+// // تعديل المسار هنا عشان Vercel يشوف الملف صح
+// const router = jsonServer.router(path.join(__dirname, "../db.json"));
+
+// app.db = router.db;
+
+// const middlewares = jsonServer.defaults({
+//   readOnly: false // السطر ده بيتضاف هنا
+// });
+
+// app.use(middlewares);
+// // ده بيخلي السيرفر يستنى شوية لو فيه ضغط
+// app.use(jsonServer.rewriter({
+//   "/api/*": "/$1"
+// }));
+// app.use(auth);
+// app.use(router);
+
+// module.exports = app;
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-const path = require("path"); // ضيفي السطر ده
+const path = require("path");
+const cors = require("cors"); // 1. استوردي مكتبة CORS
 
 const app = jsonServer.create();
-
-// تعديل المسار هنا عشان Vercel يشوف الملف صح
 const router = jsonServer.router(path.join(__dirname, "../db.json"));
 
 app.db = router.db;
 
+// 2. شغلي الـ CORS قبل أي حاجة تانية
+app.use(cors()); 
+
 const middlewares = jsonServer.defaults({
-  readOnly: false // السطر ده بيتضاف هنا
+  readOnly: false
 });
 
 app.use(middlewares);
-// ده بيخلي السيرفر يستنى شوية لو فيه ضغط
+
 app.use(jsonServer.rewriter({
   "/api/*": "/$1"
 }));
+
 app.use(auth);
 app.use(router);
 
